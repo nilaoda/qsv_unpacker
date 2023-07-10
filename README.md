6f2acfec8cce124f32fb0cdc5c658aa0d3f2ae04
nilaoda <nilaoda@live.com>
2023-07-10 19:32:12 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # qsv_unpacker
-unpack qsv container
+Unpack QSV file, output MPEGTS, JSON, M3U8 files
+
+Tested version: latest (10.6.5.7073)
+
+QSV Structure: https://github.com/btnkij/qsv2flv/tree/main/secret
+
+**Note:** The exported TS file still need to be decrypted in order to get the clear file.
+
+# requirements
+```
+pip install -r requirements.txt
+```
+
+# usage
+```
+python qsv_unpacker.py path_to_qsv.qsv
+```
+
+# screen
+![img](./0710.gif)
---
+++ qsv_unpacker.py
@@ -0,0 +1,178 @@
+import os
+import re
+import struct
+import binascii
+import base64
+import sys
+from rich.console import Console
+from rich.progress import Progress
+console = Console()
+
+def decrypt_1(buffer):
+    dict = [0x62, 0x67, 0x70, 0x79]
+    for i in range(len(buffer)):
+        j = ~i & 0x3
+        buffer[i] ^= dict[j]
+
+def decrypt_2(buffer):
+    size = len(buffer)
+    x = 0x62677079
+    for i in range(size - 1, 0, -1):
+        x = ((x << 1) | (x >> 31)) & 0xFFFFFFFF
+        x ^= buffer[i] & 0xFF
+
+    for i in range(1, size):
+        x ^= buffer[i] & 0xFF
+        x = ((x >> 1) | (x << 31)) & 0xFFFFFFFF
+        j = x % i
+        tmp = buffer[j]
+        a = buffer[i]
+        buffer[j] = tmp ^ (~a & 0xFF)
+        buffer[i] = tmp
+
+def atob(base64_string):
+    decoded_bytes = base64.b64decode(base64_string)
+    decoded_string = decoded_bytes.decode('utf-8')
+    return decoded_string
+
+def get_prop(key, text):
+    pattern = key+r'":"([^"]*)'
+    text = text.replace('\\"','斜引')
+    res = re.search(pattern, text).group(1).strip()
+    res = res.replace('斜引','\\"')
+    res = res.replace('\\n','\n')
+    return res
+
+def change_file_extension(filename, new_extension):
+    base = os.path.splitext(filename)[0]  # 获取文件名（去除后缀）
+    new_filename = base + new_extension  # 拼接新的文件名
+    return new_filename  # 重命名文件
+
+def find_bytes(byte_array, target_byte):
+    indices = []
+    for i in range(len(byte_array)):
+        if byte_array[i] == target_byte:
+            indices.append(i)
+    return indices
+
+#========================
+
+console.print('QSV Unpacker v20230710', style='bold white on cyan')
+print()
+
+# 结构体定义
+QsvHeader = struct.Struct("<10s I 16s I 32s I I Q I I")
+QsvIndex = struct.Struct("<16s Q I")
+
+if len(sys.argv) < 2:
+    console.print('Please input filename', style='bold red')
+    sys.exit(-1)
+
+if not os.path.exists(sys.argv[1]):
+    console.print('Filename not exists', style='bold red')
+    sys.exit(-1)
+
+in_file = os.path.abspath(sys.argv[1])
+
+console.print(f"[b]file   => [/b][cyan]{in_file}[/cyan]")
+
+with open(in_file, "r+b") as f:
+    # 解析前90字节
+    qheader = QsvHeader.unpack_from(f.read(QsvHeader.size))
+    signature,version,vid,_unknown1,_unknown2,_unknown3,_unknown4,xml_offset,xml_size,nb_indices = qheader
+        
+    # 仅支持version2
+    assert version == 2, "only version 2 supported!"
+
+    # vid
+    vid_hex = binascii.hexlify(vid).decode()
+    console.print(f"[b]vid    => [/b][cyan]{vid_hex}[/cyan]")
+
+    # nb_indices
+    console.print(f"[b]segs   => [/b][cyan]{nb_indices}[/cyan]")
+
+
+    # 解析索引nb_indices
+    qindices = []
+    _unknown_flag_size = (nb_indices + 7) >> 3
+    f.seek(_unknown_flag_size, 1)
+    for _ in range(nb_indices):
+        # 这里也需要解密
+        barr = bytearray(f.read(QsvIndex.size))
+        decrypt_2(barr)
+        qindices.append(QsvIndex.unpack_from(barr))
+
+    # 打印索引列表
+    for i in qindices:
+        table = bytearray(i[0])
+        console.print(f"          filename: {binascii.hexlify(table).decode()}, offset: {(i[1])}, size: {i[2]} ", style="bright_black")
+    # 打印总大小
+    console.print(f"[b]size   => [/b][cyan]{sum([i[2] for i in qindices])}[/cyan]")
+    
+    # 解密XML
+    f.seek(xml_offset)
+    xml = bytearray(f.read(xml_size))
+    decrypt_1(xml)
+    xml = xml.decode('utf-8')
+
+    # tvid
+    tvid = get_prop(key='tvid', text=xml)
+    console.print(f'[b]tvid   => [/b][cyan]{tvid}[/cyan]')
+
+    # 提取drmversion
+    drmversion = get_prop(key='drmversion', text=xml)
+    console.print(f'[b]drmver => [/b][cyan]{drmversion}[/cyan]')
+    
+    # 提取ticketdata
+    ticketdata_base64 = get_prop(key='ticketdata', text=xml)
+    ticketdata = atob(ticketdata_base64)
+    console.print(f'[b]ticket => [/b]')
+    print(ticketdata)
+
+    # 写出json
+    json_out = change_file_extension(in_file,'.json')
+    console.print(f"[b]out1   =>[/b] [cyan]{json_out}[/cyan]")
+    with open(json_out,'w',encoding='utf-8') as w:
+       w.write(xml)
+
+    # 提取m3u8
+    m3u8 = get_prop(key='m3u8', text=xml)
+    m3u8_out = change_file_extension(in_file,'.m3u8')
+    console.print(f"[b]out2   =>[/b] [cyan]{m3u8_out}[/cyan]")
+    m3u8 = m3u8.replace("#EXTM3U", f"#EXTM3U\n#DRM-TICKET:{ticketdata}")
+    with open(m3u8_out,'w',encoding='utf-8') as w:
+       w.write(m3u8)
+
+    # TS起始位置
+    offset = qindices[0][1]
+    # 写出TS文件
+    total_size = os.path.getsize(in_file) - offset
+    out_file = change_file_extension(in_file,'.ts')
+    console.print(f"[b]out3   =>[/b] [cyan]{out_file}[/cyan]")
+    with Progress() as progress, open(in_file, "rb") as in_f, open(out_file, "w+b") as out_f:
+        task = progress.add_task("[b]Copy TS...[/b]", total=total_size)
+        chunk_size = 2048
+        in_f.seek(offset)
+        while True:
+            chunk = in_f.read(chunk_size)
+            if not chunk:
+                break
+            out_f.write(chunk)
+            progress.update(task, advance=len(chunk))
+            #print(f"Progress: {progress:.2f}%\r", end='')
+
+    console.print(f"[b]Fix headers...[/b]")
+
+    # 解密加密部分
+    with open(out_file, "r+b") as in_f:
+        # 每个segment的前1024字节是加密的 需要解密
+        for seg in qindices:
+            in_f.seek(seg[1]-offset)
+            tmp = bytearray(in_f.read(1024))
+            decrypt_2(tmp)
+            in_f.seek(seg[1]-offset)
+            in_f.write(tmp)
+
+    print()
+    console.print('Done', style='bold white on green')
+
